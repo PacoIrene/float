1d8ccdd61401a25753ba43bc0fb37341d2b26f4a
PacoIrene <zhenyangchu@gmail.com>
2014-04-08 11:06:27 +0800

    Signed-off-by: PacoIrene <zhenyangchu@gmail.com>

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON("package.json"),
+		concat: {
+			domop: {
+				src:['', ''],
+				dest: 'build/float.js'
+			}
+		},
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+			}, 
+			build: {
+				src: 'build/float.js',
+				dest: 'build/float.min.js'
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+ 	// JS distribution task.
+  	grunt.registerTask('default', ['concat', 'uglify']);
+};
